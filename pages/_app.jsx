838555cb199e59cda618864f896a56a1af242521
Ameen Alade <alade.ameen@venturegardengroup.com>
2021-10-04 00:53:15 +0100

    work on fixing nav bar, banner and blog introe

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,16 +9,16 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const App = ({ Component, pageProps }) => {
-    const router = useRouter();
-    useEffect(() => {
-        const handleRouteChange = (url) => {
-            gtag.pageview(url);
-        };
-        router.events.on('routeChangeComplete', handleRouteChange);
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange);
-        };
-    }, [router.events]);
+    // const router = useRouter();
+    // useEffect(() => {
+    //     const handleRouteChange = (url) => {
+    //         gtag.pageview(url);
+    //     };
+    //     router.events.on('routeChangeComplete', handleRouteChange);
+    //     return () => {
+    //         router.events.off('routeChangeComplete', handleRouteChange);
+    //     };
+    // }, [router.events]);
 
     return (
         <>
